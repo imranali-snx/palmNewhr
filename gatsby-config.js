befc74dbb531a7fc3cc8f79352ce8e5efa034baa
imranali-snx <imranali.snx@gmail.com>
2023-01-20 07:56:03 +0500

    imraxxn

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,20 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
   ],
 }
